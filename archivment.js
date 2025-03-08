684f649c12ef9ebdd05c6c757245c97465bb8d3c
Ninja_Coder <raushanraunak2002@gmail.com>
2025-03-08 21:53:51 +0530

    css correction

--- archivment.js
+++ archivment.js
@@ -43,3 +43,23 @@ var $num = $('.card-carousel .my-card').length;
             $('.card-carousel .active').next().trigger('click');
         }
     });
+
+
+
+    // (function() {
+    //     var imgLen = document.getElementsByClassName('.my-card');
+    //     var images = imgLen.getElementsByTagName('img');
+    //     var counter = 0;
+      
+    //     if (counter <= images.length) {
+    //       setInterval(function() {
+    //         images[0].src = images[counter].src;
+    //         console.log(images[counter].src);
+    //         counter++;
+      
+    //         if (counter === images.length) {
+    //           counter = 1;
+    //         }
+    //       }, 2000);
+    //     }
+    //   });      
